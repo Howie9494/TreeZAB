224d627bc6924c46aa44df601e299e9b1f9ae4fb
Howie9494 <HowieWu94@foxmail.com>
2023-04-22 17:13:17 +0100

    import java.util.Queue instead of java.util.*,Avoid compilation slowdowns due to on-demand imports.

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/Leader.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/quorum/Leader.java
@@ -39,6 +39,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
